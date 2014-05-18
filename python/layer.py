954f50f44309a4f56b1a6d49da750a0c81aec26a
AdeB <adbrebs@gmail.com>
2014-05-18 10:02:36 +0100

    3D convolutional layers and 3D maxpooling are now implemented

--- python/layer.py
+++ python/layer.py
@@ -6,12 +6,15 @@ import numpy
 import theano
 import theano.tensor as T
 from theano.tensor.signal import downsample
-from theano.tensor.nnet import conv
+from theano.tensor.nnet import conv, conv3d2d
 
-import neurons
+from max_pool_3d import max_pool_3d
 
 
 class Layer():
+    """
+    Abstract class defining a layer of neurons.
+    """
     def __init__(self, neuron_type):
         self.w = None
         self.b = None
@@ -69,11 +72,11 @@ class LayerFullyConnected(Layer):
         return self.neuron_type.activation_function(T.dot(x, self.w) + self.b)
 
 
-class LayerConv2D(Layer):
+class LayerConv2DAbstract(Layer):
     """
-    Convolution + pooling layer
+    Abstract class defining common components of LayerConv2D and LayerConvPool2D
     """
-    def __init__(self, neuron_type, image_shape, filter_shape, poolsize=(2, 2)):
+    def __init__(self, neuron_type, image_shape, filter_shape):
         """
         Args:
             image_shape (tuple or list of length 3):
@@ -88,15 +91,101 @@ class LayerConv2D(Layer):
 
         assert image_shape[0] == filter_shape[1]
 
-        # there are "num input feature maps * filter height * filter width"
-        # inputs to each hidden unit
-        fan_in = numpy.prod(filter_shape[1:])
+        fan_in, fan_out = self.init_bounds_parameters()
 
-        # each unit in the lower layer receives a gradient from:
-        # "num output feature maps * filter height * filter width" /
-        #   pooling size
-        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /
-                   numpy.prod(poolsize))
+        # initialize weights with random weights
+        w_bound = numpy.sqrt(6. / (fan_in + fan_out))
+        self.w = theano.shared(numpy.asarray(
+            numpy.random.uniform(low=-w_bound, high=w_bound, size=filter_shape),
+            dtype=theano.config.floatX), borrow=True)
+
+        # the bias is a 1D tensor -- one bias per output feature map
+        b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
+        self.b = theano.shared(value=b_values, borrow=True)
+
+        self.params = [self.w, self.b]
+
+    def init_bounds_parameters(self):
+        raise NotImplementedError
+
+    def forward(self, x, batch_size):
+        img_batch_shape = (batch_size,) + self.image_shape
+
+        x = x.reshape(img_batch_shape)
+
+        # convolve input feature maps with filters
+        conv_out = conv.conv2d(input=x,
+                               filters=self.w,
+                               image_shape=img_batch_shape,
+                               filter_shape=self.filter_shape)
+
+        return self.forward_virtual(conv_out)
+
+    def forward_virtual(self, conv_out):
+        raise NotImplementedError
+
+
+class LayerConv2D(LayerConv2DAbstract):
+    """
+    2D Convolution layer
+    """
+    def __init__(self, neuron_type, image_shape, filter_shape):
+        LayerConv2DAbstract.__init__(self, neuron_type, image_shape, filter_shape)
+
+    def init_bounds_parameters(self):
+        fan_in = numpy.prod(self.filter_shape[1:])
+        fan_out = self.filter_shape[0] * numpy.prod(self.filter_shape[2:])
+        return fan_in, fan_out
+
+    def forward_virtual(self, conv_out):
+        return self.neuron_type.activation_function(conv_out + self.b.dimshuffle('x', 0, 'x', 'x')).flatten(2)
+
+
+class LayerConvPool2D(LayerConv2DAbstract):
+    """
+    Convolution + pooling layer. The reason for not having a separate pooling layer is that the combination of the two
+    layers can be optimized.
+    """
+    def __init__(self, neuron_type, image_shape, filter_shape, poolsize=(2, 2)):
+        self.poolsize = poolsize
+        LayerConv2DAbstract.__init__(self, neuron_type, image_shape, filter_shape)
+
+    def init_bounds_parameters(self):
+        fan_in = numpy.prod(self.filter_shape[1:])
+        fan_out = (self.filter_shape[0] * numpy.prod(self.filter_shape[2:]) / numpy.prod(self.poolsize))
+        return fan_in, fan_out
+
+    def forward_virtual(self, conv_out):
+        # downsample each feature map individually, using maxpooling
+        pooled_out = downsample.max_pool_2d(input=conv_out,
+                                            ds=self.poolsize,
+                                            ignore_border=True)
+
+        return self.neuron_type.activation_function(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x')).flatten(2)
+
+
+class LayerConvPool3D(Layer):
+    """
+    Convolution + pooling layer
+    """
+    def __init__(self, neuron_type, in_channels, in_width, in_height, in_depth,
+                 flt_channels, flt_width, flt_height, flt_depth):
+        """
+        Args:
+            image_shape (tuple or list of length 4):
+            (image depth, num input feature maps, image height, image width)
+
+            filter_shape (tuple or list of length 5):
+            (number of filters, filter depth, num input feature maps, filter height,filter width)
+        """
+        Layer.__init__(self, neuron_type)
+
+        self.image_shape = image_shape = (in_depth, in_channels, in_height, in_width)
+        self.filter_shape = filter_shape = (flt_channels, flt_depth, in_channels, flt_height, flt_width)
+
+        fan_in = in_width * in_height * in_depth
+        fan_out = flt_channels * flt_width * flt_height * flt_depth
+
         # initialize weights with random weights
         w_bound = numpy.sqrt(6. / (fan_in + fan_out))
         self.w = theano.shared(numpy.asarray(
@@ -115,80 +204,12 @@ class LayerConv2D(Layer):
         x = x.reshape(img_batch_shape)
 
         # convolve input feature maps with filters
-        conv_out = conv.conv2d(input=x,
-                               filters=self.w,
-                               image_shape=img_batch_shape,
-                               filter_shape=self.filter_shape)
+        conv_out = conv3d2d.conv3d(signals=x,
+                                   filters=self.w,
+                                   signals_shape=img_batch_shape,
+                                   filters_shape=self.filter_shape,
+                                   border_mode='valid')
 
-        # add the bias term. Since the bias is a vector (1D array), we first
-        # reshape it to a tensor of shape (1,n_filters,1,1). Each bias will
-        # thus be broadcasted across mini-batches and feature map
-        # width & height
-        return self.neuron_type.activation_function(conv_out + self.b.dimshuffle('x', 0, 'x', 'x')).flatten(2)
+        pooled_out = max_pool_3d(conv_out, (2, 2, 2), ignore_border=True)
 
-
-class LayerConvPool2D(Layer):
-    """
-    Convolution + pooling layer
-    """
-    def __init__(self, neuron_type, image_shape, filter_shape, poolsize=(2, 2)):
-        """
-        Args:
-            image_shape (tuple or list of length 3):
-            (num input feature maps, image height, image width)
-
-            filter_shape (tuple or list of length 4):
-            (number of filters, num input feature maps, filter height,filter width)
-
-            poolsize (tuple or list of length 2):
-            the downsampling (pooling) factor (#rows,#cols)
-        """
-        Layer.__init__(self, neuron_type)
-        self.image_shape = image_shape
-        self.filter_shape = filter_shape
-        self.poolsize = poolsize
-
-        assert image_shape[0] == filter_shape[1]
-
-        # there are "num input feature maps * filter height * filter width"
-        # inputs to each hidden unit
-        fan_in = numpy.prod(filter_shape[1:])
-
-        # each unit in the lower layer receives a gradient from:
-        # "num output feature maps * filter height * filter width" /
-        #   pooling size
-        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /
-                   numpy.prod(poolsize))
-        # initialize weights with random weights
-        w_bound = numpy.sqrt(6. / (fan_in + fan_out))
-        self.w = theano.shared(numpy.asarray(
-            numpy.random.uniform(low=-w_bound, high=w_bound, size=filter_shape),
-            dtype=theano.config.floatX), borrow=True)
-
-        # the bias is a 1D tensor -- one bias per output feature map
-        b_values = 0.1 + numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
-        self.b = theano.shared(value=b_values, borrow=True)
-
-        self.params = [self.w, self.b]
-
-    def forward(self, x, batch_size):
-        img_batch_shape = (batch_size,) + self.image_shape
-
-        x = x.reshape(img_batch_shape)
-
-        # convolve input feature maps with filters
-        conv_out = conv.conv2d(input=x,
-                               filters=self.w,
-                               image_shape=img_batch_shape,
-                               filter_shape=self.filter_shape)
-
-        # downsample each feature map individually, using maxpooling
-        pooled_out = downsample.max_pool_2d(input=conv_out,
-                                            ds=self.poolsize,
-                                            ignore_border=True)
-
-        # add the bias term. Since the bias is a vector (1D array), we first
-        # reshape it to a tensor of shape (1,n_filters,1,1). Each bias will
-        # thus be broadcasted across mini-batches and feature map
-        # width & height
-        return self.neuron_type.activation_function(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x')).flatten(2)
+        return self.neuron_type.activation_function(conv_out + self.b.dimshuffle('x', 'x', 0, 'x', 'x')).flatten(2)
